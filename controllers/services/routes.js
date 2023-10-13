e1e6dec5ca5218ed30acd75ac66a8527df12c980
FLYBYME <mangoraft@gmail.com>
2023-10-13 11:59:03 -0400

    work

--- controllers/services/routes.js
+++ controllers/services/routes.js
@@ -1,110 +1,26 @@
 
 
 module.exports = [
-     {
+    {
         method: 'GET',
-        path: '/company/services/:serviceID',
+        path: '/page/:slug',
         handler: {
-            render: 'services/view',
+            render: 'page.ejs',
+            layout: false,
             auth: false,
             services: [{
-                key: 'service',
-                action: {
-                    version: 1,
-                    name: 'company.services.get',
-                },
+                key: 'page',
                 rest: {
                     method: 'get',
-                    path: 'company/services/:id',
-                    endpoint: 'onehost'
+                    path: 'pages/:slug',
+                    endpoint: 'strapi'
                 },
                 params: {
-                    id: {
-                        key: 'serviceID',
+                    slug: {
+                        key: 'slug',
                         ref: 'params',
                         type: 'string'
                     }
-                },
-            }]
-        }
-    },
-    {
-        method: 'GET',
-        path: '/company/services/:serviceID/namespaces/:nsID',
-        handler: {
-            views: 'services/view',
-            services: [{
-                key: 'service',
-                action: {
-                    version: 1,
-                    name: 'company.namespace.resolve',
-                },
-                rest: {
-                    method: 'post',
-                    path: 'company/services/:id/namespaces/:namespace',
-                    endpoint: 'onehost'
-                },
-                params: {
-                    id: {
-                        key: 'serviceID',
-                        ref: 'params',
-                        type: 'string'
-                    },
-                    namespace: {
-                        key: 'nsID',
-                        ref: 'params',
-                        type: 'string'
-                    }
-                },
-            }, {
-                key: 'namespace',
-                action: {
-                    version: 1,
-                    name: 'company.namespace.get',
-                },
-                rest: {
-                    method: 'get',
-                    path: 'company/namespaces/:id',
-                    endpoint: 'onehost'
-                },
-                params: {
-                    id: {
-                        key: 'nsID',
-                        ref: 'params',
-                        type: 'string'
-                    }
-                },
-            }]
-        }
-    },
-    {
-        method: 'GET',
-        path: '/company/services',
-        handler: {
-            views: 'services/index',
-            services: [{
-                key: 'services',
-                action: {
-                    version: 1,
-                    name: 'company.services.list',
-                },
-                rest: {
-                    method: 'get',
-                    path: 'company/services',
-                    endpoint: 'onehost'
-                },
-                params: {
-                    pageSize: {
-                        ref: 'query',
-                        default: 10,
-                        type: 'number'
-                    },
-                    page: {
-                        ref: 'query',
-                        default: 1,
-                        type: 'number'
-                    },
-
                 },
             }]
         }
